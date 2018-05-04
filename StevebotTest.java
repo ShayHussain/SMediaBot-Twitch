37e80fd4d3d9de7a1d0624dc2d03d7d2b6e01a44
ShayHussain <37838290+ShayHussain@users.noreply.github.com>
2018-05-03 20:21:25 -0400

    Update StevebotTest.java

--- StevebotTest.java
+++ StevebotTest.java
@@ -1,17 +1,21 @@
+/**
+* author: Maxim Tsoy
+* tester: Haram Kim
+*/
 public class StevebotTest
 {
    public static void main(String args[])
    {
       TwitterBot twitter = new TwitterBot("I have posted from a java project, praise me");
-      //Instagrambot instagram = new Instagrambot("I have posted from a java project, praise me");
-      //AllSeeingBot allbois = new AllSeeingBot("I have posted from a java project, praise me");
+      Instagrambot instagram = new Instagrambot("I have posted from a java project, praise me");
+      AllSeeingBot allbois = new AllSeeingBot("I have posted from a java project, praise me");
       
       twitter.connect();
-      //instagram.connect();
-      //allbois.connect();
+      instagram.connect();
+      allbois.connect();
       
       twitter.post();
-      //instagram.post();
-      //allbois.post();
+      instagram.post();
+      allbois.post();
    }
 }
