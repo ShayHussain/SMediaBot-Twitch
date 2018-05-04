ec337e008f41f1720e6b30b7e0abcd2fc89cc1ca
ShayHussain <37838290+ShayHussain@users.noreply.github.com>
2018-05-03 20:45:23 -0400

    Update AllSeeingBot.java

--- AllSeeingBot.java
+++ AllSeeingBot.java
@@ -1,10 +1,19 @@
+/**
+* author: Maxim Tsoy
+* tester: Haram Kim
+*/
+
 package com.example.selen.stevebot;
 
 public class AllSeeingBot extends MediaSkeleton{
 
-    TwitterBot twitter;
-    InstagramBot instagram;
+    TwitterBot twitter; // createes the TwitterBot object
+    InstagramBot instagram; // creates the InstagramBot object
 
+    /**
+    * CONSTRUCTOR - initializes the twitter and instagram objects, assings the message to the super constructor, and runs the connect methods 
+    * for the Instagram and Twitter bots. 
+    */
     public AllSeeingBot(String message) {
         super(message);
         twitter = new TwitterBot(message);
@@ -13,11 +22,17 @@ public class AllSeeingBot extends MediaSkeleton{
         connect();
     }
 
+    /**
+    * METHOD - runs the post methods for the twitter and instagram objects.
+    */
     public void post(){
         twitter.post();
         instagram.post();
     }
 
+    /**
+    * METHOD - runs the connect methods for the twitter and instagram objects.
+    */
     public void connect(){
         twitter.connect();
         instagram.connect();
